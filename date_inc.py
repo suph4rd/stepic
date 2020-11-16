04a0db15ead86c195cc8ea1d03305266b44f16e9
suph4rd <suph4rd575@gmail.com>
2020-11-16 11:48:46 +0300

    stepic first

---
+++ date_inc.py
@@ -0,0 +1,9 @@
+from datetime import date, timedelta
+
+y, m, d = [int(x) for x in input().split()]       # Ввод
+day = int(input())
+dt = date(y, m, d)
+
+dt_td = dt + timedelta(days=day)                    # складываем дату и дни
+
+print(dt_td.year, dt_td.month, dt_td.day)
