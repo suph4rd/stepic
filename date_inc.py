8e2e9f9dcf9c696060f02590ed8c0c1553e635d5
suph4rd <suph4rd575@gmail.com>
2020-11-16 12:24:32 +0300

    4

--- date_inc.py
+++ date_inc.py
@@ -4,6 +4,6 @@ y, m, d = [int(x) for x in input().split()]       # Ввод
 day = int(input())
 dt = date(y, m, d)
 
-dt_td = dt + timedelta(days=day)                    # складываем дату и дни
+dt_td = dt + timedelta(days=day)                  # складываем дату и дни
 
 print(dt_td.year, dt_td.month, dt_td.day)
